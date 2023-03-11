3a2b46774291d8fbb8921e880f56473bbfdbfa14
Igor Tsuker <igorek888rane@gmail.com>
2023-03-11 16:47:57 +0300

    refactor

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,15 +3,15 @@ import Input from "./Input";
 import {githubApi} from "../../api/githubApi";
 
 class Form {
-    constructor(name, inputName,type,placeholder) {
+    constructor(name, inputName, type, placeholder) {
         this.name = name
         this.inputName = inputName
         this.type = type
         this.placeholder = placeholder
     }
 
-    _render(name, inputName,type,placeholder) {
-        const input = new Input(inputName,type,placeholder)
+    _render(name, inputName, type, placeholder) {
+        const input = new Input(inputName, type, placeholder)
         return createElement(`
                     <form class='form'  name = ${name}>
                         ${input.elem.outerHTML}
@@ -21,18 +21,44 @@ class Form {
     }
 
     get elem() {
-        return this._render(this.name, this.inputName,this.type,this.placeholder)
+        return this._render(this.name, this.inputName, this.type, this.placeholder)
     }
 
     async handleSubmit(e) {
         e.preventDefault()
-        const items = await githubApi(e.target[this.inputName].value)
-        console.log(items);
-        e.target[this.inputName].value = ''
+        const search = e.target[this.inputName]
+        const searchValue = e.target[this.inputName].value
+        if (searchValue) {
+            e.target[this.inputName].value = ''
+            const items = await githubApi(searchValue)
+            console.log(items);
+        } else {
+            this.setError(search, 'block', 'Enter repository name')
+        }
+    }
+
+    handleKeyDown(e) {
+        if (e.code !== 'Enter') {
+            this.setError(e.target, 'none', '')
+        }
+    }
+
+    handleBlur(e) {
+        if (!e.target.value) {
+            this.setError(e.target, 'block', 'Enter repository name')
+        }
+    }
+
+    setError(el, display, text) {
+        el.nextElementSibling.style.display = display
+        el.nextElementSibling.innerHTML = text
     }
 
     addEventListeners() {
-        document.querySelector('form').addEventListener('submit', e => this.handleSubmit(e))
+        const form = document.querySelector('form')
+        form.addEventListener('submit', e => this.handleSubmit(e))
+        form.addEventListener('keydown', e => this.handleKeyDown(e))
+        form.addEventListener('focusout', e => this.handleBlur(e))
     }
 }
 
